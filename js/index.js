ecd3f72c519045a0a550e8844e06dd1288e3bc95
Antonella Cortazzo <anttocortazzo14@gmail.com>
2024-09-09 20:41:22 -0300

    Guarda y muestra

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('buttonText').addEventListener('click', function() {
+    var inputText = document.getElementById('inputText').value;
+    localStorage.setItem('datoGuardado', inputText);
+});
